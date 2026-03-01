277725d1c3b6efd70e26f57e79934b913c2ce567
gohul4151 <gohul4151@gmail.com>
2026-03-01 18:27:38 +0530

    oauth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,34 @@ import { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
 import Login from './login';
+import Signup from './signup';
+import Home from './home';
+
 function App() {
+  const [isHome, setIsHome] = useState(false);
+  const [showLogin, setShowLogin] = useState(true);
+
+  if (isHome) {
+    return <Home />;
+  }
+
   return <>
     <h1>Kai Placement Copilot</h1>
-    <Login />
+    <div style={{ display: "flex", gap: "10px" }}>
+      <button onClick={() => { setShowLogin(true) }}>Login</button>
+      <button onClick={() => { setShowLogin(false) }}>Signup</button>
+    </div>
+
+    {showLogin ?
+      <Login onAuthSuccess={() => setIsHome(true)} /> :
+      <Signup onAuthSuccess={() => setIsHome(true)} />
+    }
+
     <GoogleLogin
       onSuccess={res => {
         console.log(res);
         console.log(jwtDecode(res.credential));
+        setIsHome(true);
       }}
       onError={() => {
         console.log('Login Failed');
@@ -18,4 +38,4 @@ function App() {
   </>
 }
 
-export default App
+export default App;
--- frontend/src/signup.jsx
+++ frontend/src/signup.jsx
@@ -1,11 +1,12 @@
-import { useState, useRef } from "react";
+import { useState, useRef } from 'react';
 
-function Login({ onAuthSuccess }) {
+function Signup({ onAuthSuccess }) {
+    const name = useRef("");
     const email = useRef("");
     const password = useRef("");
     const [error, setError] = useState("");
-    async function login() {
-        const res = await fetch("http://localhost:3000/login", {
+    async function signup() {
+        const res = await fetch("http://localhost:3000/signup", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -13,6 +14,7 @@ function Login({ onAuthSuccess }) {
             },
             credentials: "include",
             body: JSON.stringify({
+                name: name.current.value,
                 email: email.current.value,
                 password: password.current.value
             })
@@ -21,26 +23,25 @@ function Login({ onAuthSuccess }) {
         console.log(data);
         setError(data.message);
 
-        // If login is successful, call the transition function
         if (res.ok) {
             onAuthSuccess();
         }
     }
     return <>
-        <div>
-            <h3>Login</h3>
-            <p>email</p>
-            <input type="text" ref={email} />
-            <p>password</p>
-            <input type="password" ref={password} />
-            <br />
-            <br />
-            <br />
-            <button onClick={() => { login() }}>Login</button>
-            <br />
-            {error}
-        </div>
+        <h3>Signup</h3>
+        <p>Name</p>
+        <input type="text" ref={name} />
+        <p>Email</p>
+        <input type="email" ref={email} />
+        <p>Password</p>
+        <input type="password" ref={password} />
+        <br />
+        <br />
+        <br />
+        <button onClick={() => { signup() }}>Signup</button>
+        <br />
+        {error}
     </>
 }
 
-export default Login;
+export default Signup;
